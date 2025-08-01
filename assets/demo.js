1a4c60f9ad3493304be612a495731839a1edf78a
Valor Naram <valinora@gmx.net>
2025-08-01 18:23:45 +0200

    chore: use const instead of let to declare variables which don't need to be changed

--- assets/demo.js
+++ assets/demo.js
@@ -1,9 +1,9 @@
-let htmlTableElem = document.getElementsByTagName("table")[0];
-let simpleTableToggle = document.getElementsByTagName("input")[0];
+const htmlTableElem = document.getElementsByTagName("table")[0];
+const simpleTableToggle = document.getElementsByTagName("input")[0];
 
-let simpleHTMLTables = SimplifyHTMLTable.Util.generateSimpleHTMLTablesOutOfComplicatedHTMLTable(htmlTableElem);
+const simpleHTMLTables = SimplifyHTMLTable.Util.generateSimpleHTMLTablesOutOfComplicatedHTMLTable(htmlTableElem);
 
-let htmlTableRef = htmlTableElem.getAttribute(SimplifyHTMLTable.Util.tableReferenceForLinkAttribute);
+const htmlTableRef = htmlTableElem.getAttribute(SimplifyHTMLTable.Util.tableReferenceForLinkAttribute);
 
 for (simpleHTMLTable of simpleHTMLTables) {
 	simpleHTMLTable.setAttribute("hidden", "true");
